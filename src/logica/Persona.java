62284028686e4f54a821195c4c07bbd48169bbda
Harold Smith <hsgomez@uniqundio.edu.co>
2023-03-30 09:36:56 -0500

    Se hace el constructor de la clase

---
+++ src/logica/Persona.java
@@ -0,0 +1,13 @@
+package logica;
+
+public class Persona {
+	
+	private String nombre;
+	private String email;
+	public Persona(String nombre, String email) {
+		this.nombre = nombre;
+		this.email = email;
+	}
+	
+	
+}
