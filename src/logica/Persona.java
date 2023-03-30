f9d0c49de8b3344cc5e4e90747b7e0211b426b34
Harold Smith <hsgomez@uniqundio.edu.co>
2023-03-30 09:42:28 -0500

    Se crean getter y setter

--- src/logica/Persona.java
+++ src/logica/Persona.java
@@ -8,6 +8,20 @@ public class Persona {
 		this.nombre = nombre;
 		this.email = email;
 	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	
 	
 	
 }
