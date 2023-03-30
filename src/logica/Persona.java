3fe352eb7947e735bb610352b5d9cadc50033fb2
Harold Smith <hsgomez@uniqundio.edu.co>
2023-03-30 10:16:49 -0500

    se crea metodo toString

--- src/logica/Persona.java
+++ src/logica/Persona.java
@@ -1,27 +1,36 @@
 package logica;
 
 public class Persona {
-	
+
 	private String nombre;
 	private String email;
+
 	public Persona(String nombre, String email) {
 		this.nombre = nombre;
 		this.email = email;
 	}
+
 	public String getNombre() {
 		return nombre;
 	}
+
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
+
 	public String getEmail() {
 		return email;
 	}
+
 	public void setEmail(String email) {
 		this.email = email;
 	}
+
+	@Override
+	public String toString() {
+		return "Nombre=" + nombre + "\n Email=" + email;
+	}
 	
 	
-	
-	
+
 }
